9d3853726d1e12ed94d538cf025a8ae73cefb821
Colby Dennis <=>
2025-09-18 23:35:27 -0400

    logic for menu button

--- bug-farmer/main.lua
+++ bug-farmer/main.lua
@@ -1,14 +1,18 @@
-local mainMenu = require("bug-farmer.menu.menu")
+package.path = package.path .. ';../bugfarmer;../bugfarmer/menu;'
+local mainMenu = require("menu.menu")
+
+gameState = {}
+
+gameState.currentScreen = "menu"
 
 function love.load()
   world = love.physics.newWorld(0, 0, 0)
   mainMenu.initCanvas(800,600)
   mainMenu.drawMenu(world)
-  print(mainMenu)
 end
 
 function love.update(dt)
-
+  mainMenu.updateMenuState(dt)
 end
 
 function love.draw()
--- bug-farmer/menu/menu.lua
+++ bug-farmer/menu/menu.lua
@@ -1,5 +1,12 @@
 local mainMenu = {}
 mainMenu.objects = {}
+mainMenu.state = {}
+
+mainMenu.state.mouseDown = false
+mainMenu.state.mouseUp = true
+mainMenu.state.pressPlay = false
+mainMenu.state.pressSettings = false
+
 --@param dimX number dimension width
 --@param dimY number dimension height
 --@return creates a new canvas
@@ -25,34 +32,72 @@ function mainMenu.drawMenu(world)
   mainMenu.objects.playButton = {}
   local playButton = mainMenu.objects.playButton
   playButton.image = love.graphics.newImage("menu/assets/playButton.png")
-  playButton.x = mainMenu.dimX * .125
-  playButton.y = mainMenu.dimY * .7083
+  playButton.x = mainMenu.dimX * .21875
+  playButton.y = mainMenu.dimY * .791667
+  playButton.height = 100
+  playButton.width = 150
   playButton.body = love.physics.newBody(world, playButton.x, playButton.y)
   playButton.shape = love.physics.newRectangleShape(150, 100)
   playButton.fixture = love.physics.newFixture(playButton.body, playButton.shape, 1)
-  love.graphics.draw(playButton.image, playButton.x, playButton.y)
+  mainMenu.objects.playButton = playButton
+  love.graphics.draw(playButton.image, playButton.x, playButton.y, 0, 1, 1, playButton.width / 2, playButton.height / 2)
 
   mainMenu.objects.menuButton = {}
   local menuButton = mainMenu.objects.menuButton
   menuButton.image = love.graphics.newImage("menu/assets/menuButton.png")
-  menuButton.x = mainMenu.dimX * .40625
-  menuButton.y = mainMenu.dimY * .7083
+  menuButton.x = mainMenu.dimX * .5
+  menuButton.y = mainMenu.dimY * .791667
+  menuButton.height = 100
+  menuButton.width = 150
   menuButton.body = love.physics.newBody(world, menuButton.x, menuButton.y)
   menuButton.shape = love.physics.newRectangleShape(150, 100)
   menuButton.fixture = love.physics.newFixture(menuButton.body, menuButton.shape, 1)
-  love.graphics.draw(menuButton.image, menuButton.x, menuButton.y)
+  mainMenu.objects.menuButton = menuButton
+  love.graphics.draw(menuButton.image, menuButton.x, menuButton.y, 0, 1, 1, menuButton.width / 2, menuButton.height / 2)
 
   mainMenu.objects.exitButton = {}
   local exitButton = mainMenu.objects.exitButton
   exitButton.image = love.graphics.newImage("menu/assets/exitButton.png")
-  exitButton.x = mainMenu.dimX * .6875
-  exitButton.y = mainMenu.dimY * .7083
+  exitButton.x = mainMenu.dimX * .78125
+  exitButton.y = mainMenu.dimY * .791667
+  exitButton.height = 100
+  exitButton.width = 150
   exitButton.body = love.physics.newBody(world, exitButton.x, exitButton.y)
-  exitButton.shape = love.physics.newRectangleShape(150, 100)
+  exitButton.shape = love.physics.newRectangleShape(exitButton.width, exitButton.height)
   exitButton.fixture = love.physics.newFixture(exitButton.body, exitButton.shape, 1)
-  love.graphics.draw(exitButton.image, exitButton.x, exitButton.y)
+  mainMenu.objects.exitButton = exitButton
+  love.graphics.draw(exitButton.image, exitButton.x, exitButton.y, 0, 1, 1, exitButton.width / 2, exitButton.height / 2)
 
   love.graphics.setCanvas()
+end
+
+function mainMenu.updateMenuState(dt) 
+  -- if mouse is up, allow mouseDown
+  mainMenu.state.mouseDown = love.mouse.isDown(1)
+  if mainMenu.state.mouseDown and mainMenu.state.mouseUp then
+    handleUpdate()
+  end
+  mainMenu.state.mouseUp = not love.mouse.isDown(1)
+end
+
+function handleUpdate()
+  local cursorX, cursorY = love.mouse.getPosition()
+
+  -- check each box
+  if mainMenu.objects.exitButton.fixture:testPoint(cursorX, cursorY) then
+    love.event.quit()
+  end
+
+  if mainMenu.objects.playButton.fixture:testPoint(cursorX, cursorY) then
+    -- how do i manage screen switch? global variable from here?
+    mainMenu.state.pressPlay = true
+    return
+  end
+
+  if mainMenu.objects.menuButton.fixture:testPoint(cursorX, cursorY) then
+    mainMenu.state.pressSettings = true
+    return
+  end
 end
 
 return mainMenu
